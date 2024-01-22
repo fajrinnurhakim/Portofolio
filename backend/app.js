9121a2ffff7dc13a17f49deb8a24103914d46c57
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2024-01-22 20:42:20 +0700

    change name dir

--- backend/app.js
+++ backend/app.js
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use(router);
 
 app.listen(port, () => {
-    console.log(` app listening on port 8000 ${port}`);
+    console.log(` app listening on port ${port}`);
 });
