e0cd093b28f183f07b2ad9c878b3938fc8156346
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2024-02-28 00:22:38 +0700

    add: feature profile page

---
+++ frontend/src/hooks/profile.jsx
@@ -0,0 +1,155 @@
+import { useState } from "react";
+import {
+    fetchProfiles,
+    createProfile,
+    updateProfile,
+    deleteProfile,
+} from "../utils/fetchProfile";
+
+function stateProfile() {
+    const [profiles, setProfiles] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [newProfileData, setNewProfileData] = useState({
+        name: "",
+        address: "",
+        main_role: "",
+        description: "",
+        link_cv: "",
+        image: "",
+        image_two: "",
+        year_experience: "",
+        tech_stack: "",
+        success_project: "",
+    });
+    const [updateIndex, setUpdateIndex] = useState(null);
+    const [updateProfileData, setUpdateProfileData] = useState({
+        name: "",
+        address: "",
+        main_role: "",
+        description: "",
+        link_cv: "",
+        image: "",
+        image_two: "",
+        year_experience: "",
+        tech_stack: "",
+        success_project: "",
+    });
+
+    const loadProfiles = async () => {
+        try {
+            const data = await fetchProfiles();
+            setProfiles(data);
+        } catch (error) {
+            console.error("Error loading profiles:", error);
+        }
+    };
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setNewProfileData({
+            ...newProfileData,
+            [name]: value,
+        });
+    };
+
+    const handleUpdateInputChange = (event) => {
+        const { name, value } = event.target;
+        setUpdateProfileData({
+            ...updateProfileData,
+            [name]: value,
+        });
+    };
+
+    const handleCreateProfile = async () => {
+        try {
+            await createProfile(newProfileData);
+            setShowModal(false);
+            loadProfiles();
+            setNewProfileData({
+                name: "",
+                address: "",
+                main_role: "",
+                description: "",
+                link_cv: "",
+                image: "",
+                image_two: "",
+                year_experience: "",
+                tech_stack: "",
+                success_project: "",
+            });
+        } catch (error) {
+            console.error("Error creating profile:", error);
+        }
+    };
+
+    const handleUpdateProfile = async () => {
+        try {
+            const id = profiles[updateIndex].id;
+            const updatedData = updateProfileData;
+            await updateProfile(id, updatedData);
+            setShowModal(false);
+            loadProfiles();
+            setUpdateIndex(null);
+            setUpdateProfileData({
+                name: "",
+                address: "",
+                main_role: "",
+                description: "",
+                link_cv: "",
+                image: "",
+                image_two: "",
+                year_experience: "",
+                tech_stack: "",
+                success_project: "",
+            });
+        } catch (error) {
+            console.error("Error updating profile:", error);
+        }
+    };
+
+    const handleOpenUpdateModal = (index) => {
+        setUpdateIndex(index);
+        setShowModal(true);
+        setUpdateProfileData({
+            name: profiles[index].name,
+            address: profiles[index].address,
+            main_role: profiles[index].main_role,
+            description: profiles[index].description,
+            link_cv: profiles[index].link_cv,
+            image: profiles[index].image,
+            image_two: profiles[index].image_two,
+            year_experience: profiles[index].year_experience,
+            tech_stack: profiles[index].tech_stack,
+            success_project: profiles[index].success_project,
+        });
+    };
+
+    const handleDeleteProfile = async (id) => {
+        try {
+            await deleteProfile(id);
+            loadProfiles();
+        } catch (error) {
+            console.error("Error deleting profile:", error);
+        }
+    };
+
+    return {
+        profiles,
+        showModal,
+        newProfileData,
+        updateIndex,
+        updateProfileData,
+        loadProfiles,
+        handleInputChange,
+        handleUpdateInputChange,
+        handleCreateProfile,
+        handleUpdateProfile,
+        handleOpenUpdateModal,
+        handleDeleteProfile,
+        setShowModal,
+        setUpdateIndex,
+        setUpdateProfileData,
+    };
+}
+
+export default stateProfile;
---
+++ frontend/src/utils/fetchProfile.js
@@ -0,0 +1,94 @@
+import axios from "axios";
+const baseUrl = import.meta.env.VITE_REACT_API_URL;
+import Swal from "sweetalert2";
+
+export const fetchProfiles = async () => {
+    try {
+        const response = await axios.get(`${baseUrl}/profiles`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching profiles:", error);
+        throw error;
+    }
+};
+
+export const createProfile = async (profileData) => {
+    try {
+        const response = await axios.post(
+            `${baseUrl}/profiles`,
+            profileData
+        );
+        Swal.fire({
+            icon: "success",
+            title: "Success",
+            text: `Create Profile Successfully`,
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error adding profile:", error);
+        Swal.fire({
+            icon: "error",
+            title: "Error!",
+            text: `${error.message}`,
+        });
+        throw error;
+    }
+};
+
+export const updateProfile = async (profileId, updatedProfileData) => {
+    try {
+        const response = await axios.put(
+            `${baseUrl}/profiles/${profileId}`,
+            updatedProfileData
+        );
+        Swal.fire({
+            icon: "success",
+            title: "Success",
+            text: `Update Profile Successfully`,
+        });
+        return response.data;
+    } catch (error) {
+        Swal.fire({
+            icon: "error",
+            title: "Error!",
+            text: `${error.message}`,
+        });
+        throw error;
+    }
+};
+
+export const deleteProfile = async (profileId) => {
+    try {
+        const response = await axios.delete(
+            `${baseUrl}/profiles/${profileId}`
+        );
+        Swal.fire({
+            icon: "success",
+            title: "Success",
+            text: `Delete Profile Successfully`,
+        });
+        return response.data;
+    } catch (error) {
+        Swal.fire({
+            icon: "error",
+            title: "Error!",
+            text: `${error.message}`,
+        });
+        throw error;
+    }
+};
+
+export const fetchProfileById = async (profileId) => {
+    try {
+        const response = await axios.get(
+            `${baseUrl}/profiles/${profileId}`
+        );
+        return response.data;
+    } catch (error) {
+        console.error(
+            `Error fetching profile with ID ${profileId}:`,
+            error
+        );
+        throw error;
+    }
+};
