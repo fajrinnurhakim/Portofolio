9b74bab08f1bf31fabdd7766150aa232d70c0893
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2024-03-12 17:49:43 +0700

    add: portofolio  modal

--- frontend/src/components/portofolioSection.jsx
+++ frontend/src/components/portofolioSection.jsx
@@ -1,13 +1,23 @@
 import React, { useEffect } from "react";
 import statePortofolio from "../hooks/portofolio";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const PortofolioSection = () => {
     const { portofolios, loadPortofolios } = statePortofolio();
+    const [selectedPortofolio, setSelectedPortofolio] = useState(null);
 
     useEffect(() => {
         loadPortofolios();
     }, []);
+
+    const openModal = (portofolio) => {
+        setSelectedPortofolio(portofolio);
+    };
+
+    const closeModal = () => {
+        setSelectedPortofolio(null);
+    };
 
     return (
         <div className="container flex items-center h-auto px-5 py-24 mx-auto space-y-5">
@@ -30,7 +40,10 @@ const PortofolioSection = () => {
                                     />
                                 </figure>
                                 <div className="p-4 card-body">
-                                    <h2 className="text-base font-bold md:card-title">
+                                    <h2
+                                        className="text-base font-bold cursor-pointer md:card-title link-hover link"
+                                        onClick={() => openModal(portofolio)}
+                                    >
                                         {portofolio.portofolio_name}
                                     </h2>
                                     <div className="flex space-x-2">
@@ -87,6 +100,60 @@ const PortofolioSection = () => {
                     </div>
                 </div>
             </div>
+            {selectedPortofolio && (
+                <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50">
+                    <div className="p-4 space-y-2 rounded-lg bg-base-300 w-96 card">
+                        <figure>
+                            <img
+                                src={selectedPortofolio.portofolio_image}
+                                alt="portofolioImage"
+                                className="w-full h-48"
+                            />
+                        </figure>
+                        <h2 className="text-xl font-bold ">
+                            {selectedPortofolio.portofolio_name}
+                        </h2>
+                        <div className="flex space-x-2">
+                            <img
+                                src={selectedPortofolio.tech_stack1}
+                                alt="portofolio1"
+                                className="h-5"
+                            />
+                            <img
+                                src={selectedPortofolio.tech_stack2}
+                                alt="portofolio1"
+                                className="h-5"
+                            />
+                            <img
+                                src={selectedPortofolio.tech_stack3}
+                                alt="portofolio1"
+                                className="h-5"
+                            />
+                        </div>
+                        <p>{selectedPortofolio.description}</p>
+                        <div className="flex space-x-1">
+                            <a
+                                href={selectedPortofolio.link_github}
+                                className="w-1/2 btn btn-square"
+                            >
+                                Repository <i className="fa-solid fa-code"></i>
+                            </a>
+                            <a
+                                href={selectedPortofolio.link_demo}
+                                className="w-1/2 btn btn-square"
+                            >
+                                Preview <i class="fa-solid fa-link"></i>
+                            </a>
+                        </div>
+                        <button
+                            onClick={closeModal}
+                            className="w-full btn btn-square"
+                        >
+                            Close
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
--- frontend/src/pages/postofolioPage/index.jsx
+++ frontend/src/pages/postofolioPage/index.jsx
@@ -2,16 +2,26 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import statePortofolio from "../../hooks/portofolio";
+import { useState } from "react";
 
 const PortofolioPage = () => {
     const { portofolios, loadPortofolios } = statePortofolio();
+    const [selectedPortofolio, setSelectedPortofolio] = useState(null);
 
     useEffect(() => {
         loadPortofolios();
     }, []);
 
+    const openModal = (portofolio) => {
+        setSelectedPortofolio(portofolio);
+    };
+
+    const closeModal = () => {
+        setSelectedPortofolio(null);
+    };
+
     return (
-        <div className="container flex items-center h-auto px-5 py-24 mx-auto space-y-5">
+        <div className="container flex items-center h-auto px-5 py-24 mx-auto">
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>Portofolios | Fajrin Nurhakim</title>
@@ -31,7 +41,7 @@ const PortofolioPage = () => {
                         {portofolios.map((portofolio, index) => (
                             <div
                                 key={index}
-                                className="shadow-xl card bg-base-300"
+                                className="shadow-xl card bg-base-100"
                             >
                                 <figure>
                                     <img
@@ -41,7 +51,10 @@ const PortofolioPage = () => {
                                     />
                                 </figure>
                                 <div className="p-4 card-body">
-                                    <h2 className="text-base font-bold md:card-title">
+                                    <h2
+                                        className="text-base font-bold cursor-pointer md:card-title link-hover link"
+                                        onClick={() => openModal(portofolio)}
+                                    >
                                         {portofolio.portofolio_name}
                                     </h2>
                                     <div className="flex space-x-2">
@@ -92,6 +105,60 @@ const PortofolioPage = () => {
                     </div>
                 </div>
             </div>
+            {selectedPortofolio && (
+                <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50">
+                    <div className="p-4 space-y-2 rounded-lg bg-base-300 w-96 card">
+                        <figure>
+                            <img
+                                src={selectedPortofolio.portofolio_image}
+                                alt="portofolioImage"
+                                className="w-full h-48"
+                            />
+                        </figure>
+                        <h2 className="text-base font-bold">
+                            {selectedPortofolio.portofolio_name}
+                        </h2>
+                        <div className="flex space-x-2">
+                            <img
+                                src={selectedPortofolio.tech_stack1}
+                                alt="portofolio1"
+                                className="h-5"
+                            />
+                            <img
+                                src={selectedPortofolio.tech_stack2}
+                                alt="portofolio1"
+                                className="h-5"
+                            />
+                            <img
+                                src={selectedPortofolio.tech_stack3}
+                                alt="portofolio1"
+                                className="h-5"
+                            />
+                        </div>
+                        <p>{selectedPortofolio.description}</p>
+                        <div className="flex space-x-1">
+                            <a
+                                href={selectedPortofolio.link_github}
+                                className="w-1/2 btn btn-square"
+                            >
+                                Repository <i className="fa-solid fa-code"></i>
+                            </a>
+                            <a
+                                href={selectedPortofolio.link_demo}
+                                className="w-1/2 btn btn-square"
+                            >
+                                Preview <i class="fa-solid fa-link"></i>
+                            </a>
+                        </div>
+                        <button
+                            onClick={closeModal}
+                            className="w-full btn btn-square"
+                        >
+                            Close
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
