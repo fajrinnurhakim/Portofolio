72df17daf6cd68e4d4ee4304eca8d37553e4586c
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2024-04-21 21:59:19 +0700

    add image

--- frontend/src/components/educationSection.jsx
+++ frontend/src/components/educationSection.jsx
@@ -183,7 +183,7 @@ const EducationSection = () => {
                                                             xmlns="http://www.w3.org/2000/svg"
                                                             viewBox="0 0 20 20"
                                                             fill="currentColor"
-                                                            className="w-5 h-5"
+                                                            className="w-5 h-5" 
                                                         >
                                                             <path
                                                                 fillRule="evenodd"
